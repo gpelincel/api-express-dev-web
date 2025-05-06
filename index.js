c7d57360428ba0ca852b41fab0d67cb93e23391e
gpelincel <gabrielpelincel@gmail.com>
2025-05-06 13:45:26 -0300

    Fix: cors

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors'); // importe o cors
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors()); // habilite o cors aqui
+
 app.get('/datetime', (req, res) => {
   const now = new Date();
-  res.json({
-    date: now.toLocaleDateString(),
-    time: now.toLocaleTimeString(),
-  });
+  const date = now.toLocaleDateString('pt-BR');
+  const time = now.toLocaleTimeString('pt-BR');
+  res.json({ date, time });
 });
 
 app.listen(port, () => {
-  console.log(`API rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
